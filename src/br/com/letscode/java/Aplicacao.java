b99904807aa5963b664b42c807106101c7160b7e
Roger <rogerio.alonso@soulasalle.com.br>
2021-09-24 20:59:41 -0300

    Create the project

---
+++ src/br/com/letscode/java/Aplicacao.java
@@ -0,0 +1,11 @@
+package br.com.letscode.java;
+
+public class Aplicacao {
+
+    public static void main(String[] args) {
+
+        //Menu do app
+
+
+    }
+}
---
+++ src/br/com/letscode/java/classes/Cliente.java
@@ -0,0 +1,4 @@
+package br.com.letscode.java.classes;
+
+public class Cliente {
+}
