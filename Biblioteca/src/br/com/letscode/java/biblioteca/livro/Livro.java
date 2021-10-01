68ccf17b00759bb38d241302ea3f9c63fe6dcd93
GuerreroWifi <72557257+GuerreroWifi@users.noreply.github.com>
2021-09-30 21:30:19 -0300

    Update Livro.java

--- Biblioteca/src/br/com/letscode/java/biblioteca/livro/Livro.java
+++ Biblioteca/src/br/com/letscode/java/biblioteca/livro/Livro.java
@@ -1,4 +1,20 @@
 package br.com.letscode.java.biblioteca.livro;
 
 public class Livro {
+    private int isbn;
+    private String titulo;
+    private String autor;
+    private String editor;
+
+    public Livro( int isbn, String titulo, String autor, String editor){
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editor = editor;
+    }
+    public  String toString(){
+        return "Livro: "+" isbn= "+this.isbn+"; titulo= "
+                +this.titulo+"; autor= "+this.autor+"; editor= "
+                +this.editor+".";
+    }
 }
