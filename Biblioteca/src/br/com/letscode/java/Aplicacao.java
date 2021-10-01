12bf790e63d9378f8c79149e50adc8ec5735fa29
GuerreroWifi <72557257+GuerreroWifi@users.noreply.github.com>
2021-09-30 21:42:12 -0300

    Update Aplicacao.java

--- Biblioteca/src/br/com/letscode/java/Aplicacao.java
+++ Biblioteca/src/br/com/letscode/java/Aplicacao.java
@@ -5,12 +5,29 @@ import br.com.letscode.java.biblioteca.clientes.ClienteAluno;
 import br.com.letscode.java.biblioteca.clientes.ClienteProfessor;
 import br.com.letscode.java.biblioteca.clientes.TipoCliente;
 import br.com.letscode.java.biblioteca.livro.Emprestimo;
+import br.com.letscode.java.biblioteca.livro.Livro;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Aplicacao {
 
     public static void main(String[] args) {
+        
+        ArrayList<Livro> listaLivros = new ArrayList<>();
+
+        Livro dub = new Livro(15566, "Diário de um banana", "Jeff Kinney", "V&R");
+        Livro pj = new Livro(14477, "Percy Jackson", "Rick Riordan", "Intrínseca");
+        Livro hp = new Livro(33009, "Harry Potter", "J. K. Rowling", "Rocco");
+        Livro sa = new Livro(65757, "Senhor dos anéis", "J. K. K. Tolkien", "Martins Fontes");
+        Livro gow = new Livro(90987, "God of War", "Matthew Stover", "Europa");
+
+        listaLivros.add(dub);
+        listaLivros.add(pj);
+        listaLivros.add(hp);
+        listaLivros.add(sa);
+        listaLivros.add(gow);
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite se o empréstimo pertence a um professor ou a um aluno: ");
         String tpCliente = sc.nextLine();
